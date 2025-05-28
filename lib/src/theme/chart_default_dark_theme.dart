40ff0f65fcb8e8c8a5cdb7cf80e7cbd1b8999dcb
Loay AlBarmawi <l.albarmawi@ingotbrokers.com>
2025-05-28 08:43:44 +0300

    test

--- lib/src/theme/chart_default_dark_theme.dart
+++ lib/src/theme/chart_default_dark_theme.dart
@@ -84,7 +84,7 @@ class ChartDefaultDarkTheme extends ChartDefaultTheme {
       DarkThemeColors.crosshairLineResponsiveUpperLineGradientStart;
 
   @override
-  Color get crosshairInformationBoxBackgroundColor => const Color(0xFF323738);
+  Color get crosshairInformationBoxBackgroundColor => Colors.red;// const Color(0xFF323738);
 
   @override
   Color get currentSpotDotColor => DarkThemeColors.currentSpotDotColor;
