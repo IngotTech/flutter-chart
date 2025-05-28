903e8ceb860a80c375100c7b178527c544060d86
Loay AlBarmawi <l.albarmawi@ingotbrokers.com>
2025-05-28 10:00:35 +0300

    added style to indicators

--- lib/src/deriv_chart/chart/data_visualization/chart_series/line_series/line_series.dart
+++ lib/src/deriv_chart/chart/data_visualization/chart_series/line_series/line_series.dart
@@ -24,7 +24,7 @@ class LineSeries extends DataSeries<Tick> {
           style: style,
           lastTickIndicatorStyle: lastTickIndicatorStyle,
         );
-    double glowIntensity = 0.0;
+  
 
   @override
   SeriesPainter<DataSeries<Tick>> createPainter() => LinePainter(
